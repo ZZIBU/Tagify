7e234948be1a0008bb8f95ef84b2ad40db63695e
이제호 <78259314+jhl8109@users.noreply.github.com>
2024-06-07 23:19:55 +0900

    set up kotlin sdk options

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation(kotlin("stdlib-jdk8"))
 }
 
 kotlin {
