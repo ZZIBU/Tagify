44bea064d3b3b7cdc031c498675531847c94e8d3
이제호 <78259314+jhl8109@users.noreply.github.com>
2024-06-09 21:16:17 +0900

    정상 동작을 위한 TagRepository / TagEntity 개발(TDD - Green stage)
    - 실제 Spring 동작 환경의 테스트코드 작성
    - ES Repository 개발
    - ES Document entity 개발
    - TestDouble Fake 적용
    - uploadDate auditing 설정 추가
    src/main/resources/application.yml

---
+++ src/main/kotlin/zzibu/jeho/tagify/repository/TagRepository.kt
@@ -0,0 +1,10 @@
+package zzibu.jeho.tagify.repository
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
+import zzibu.jeho.tagify.entity.TagInfo
+import java.util.*
+
+interface TagRepository : ElasticsearchRepository<TagInfo,String> {
+    override fun findById(id: String): Optional<TagInfo>
+    override fun <S : TagInfo?> save(entity: S & Any): S & Any
+}
---
+++ src/test/kotlin/zzibu/jeho/tagify/repository/FakeTagRepository.kt
@@ -0,0 +1,112 @@
+package zzibu.jeho.tagify.repository
+
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.elasticsearch.core.RefreshPolicy
+import zzibu.jeho.tagify.entity.TagInfo
+import java.time.LocalDateTime
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+
+class FakeTagRepository : TagRepository{
+    private val storage = ConcurrentHashMap<String, TagInfo>()
+    private var idCounter = 0L;
+
+    // ElasticsearchRepository가 java로 구현되어 TagInfo?가 아닌 Optional을 사용
+    override fun findById(id: String): Optional<TagInfo> {
+        return Optional.ofNullable(storage[id])
+    }
+
+    override fun <S : TagInfo?> save(entity: S & Any): S & Any {
+        val id = entity?.id ?: (++idCounter).toString()
+        val tag = entity!!.copy(id = id, uploadDate = LocalDateTime.now())
+        storage[id] = tag
+        return tag as (S & Any)
+    }
+
+    // ---------------- Not yet implemented ----------------
+
+    override fun findAll(sort: Sort): MutableIterable<TagInfo> {
+        TODO("Not yet implemented")
+    }
+
+    override fun findAll(pageable: Pageable): Page<TagInfo> {
+        TODO("Not yet implemented")
+    }
+
+    override fun findAll(): MutableIterable<TagInfo> {
+        TODO("Not yet implemented")
+    }
+
+    override fun <S : TagInfo?> save(entity: S & Any, refreshPolicy: RefreshPolicy?): S & Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun <S : TagInfo?> saveAll(
+        entities: MutableIterable<S>,
+        refreshPolicy: RefreshPolicy?
+    ): MutableIterable<S> {
+        TODO("Not yet implemented")
+    }
+
+    override fun <S : TagInfo?> saveAll(entities: MutableIterable<S>): MutableIterable<S> {
+        TODO("Not yet implemented")
+    }
+
+    override fun existsById(id: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun findAllById(ids: MutableIterable<String>): MutableIterable<TagInfo> {
+        TODO("Not yet implemented")
+    }
+
+    override fun count(): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteById(id: String, refreshPolicy: RefreshPolicy?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteById(id: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(entity: TagInfo, refreshPolicy: RefreshPolicy?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(entity: TagInfo) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteAllById(ids: MutableIterable<String>, refreshPolicy: RefreshPolicy?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteAllById(ids: MutableIterable<String>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteAll(entities: MutableIterable<TagInfo>, refreshPolicy: RefreshPolicy?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteAll(refreshPolicy: RefreshPolicy?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteAll(entities: MutableIterable<TagInfo>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteAll() {
+        TODO("Not yet implemented")
+    }
+
+    override fun searchSimilar(entity: TagInfo, fields: Array<out String>?, pageable: Pageable): Page<TagInfo> {
+        TODO("Not yet implemented")
+    }
+}
