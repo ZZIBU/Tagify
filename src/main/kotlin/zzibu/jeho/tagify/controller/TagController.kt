d355eee3d04499ddd7559d5e71c5d64174b20790
이제호 <78259314+jhl8109@users.noreply.github.com>
2024-06-26 23:28:38 +0900

    [#1] refactor: #1 review 반영 - 불필요한 코드 제거 및 CoFiler API URI와 공통화

--- src/main/kotlin/zzibu/jeho/tagify/controller/TagController.kt
+++ src/main/kotlin/zzibu/jeho/tagify/controller/TagController.kt
@@ -1,6 +1,7 @@
 package zzibu.jeho.tagify.controller
 
-import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -9,15 +10,18 @@ import org.springframework.web.multipart.MultipartFile
 import zzibu.jeho.tagify.service.TagService
 
 @RestController
-@RequestMapping("/api/tags")
-class TagController(@Autowired private val tagService: TagService) {
+@RequestMapping("/api/v1/tag")
+class TagController(private val tagService: TagService) {
 
-    @PostMapping("/generate/image")
+    @PostMapping("/image")
     fun generateTags(
         @RequestParam("image") image: MultipartFile,
 
-    ): List<String> {
-        return tagService.generateTagByImage(image)
+    ): ResponseEntity<List<String>> {
+        return ResponseEntity(
+            tagService.generateTagByImage(image),
+            HttpStatus.OK
+        )
     }
 
 //    @PostMapping("/generate/url")
