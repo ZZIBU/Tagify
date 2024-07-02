cce13aad19672fbc81e55503231f5f3105252b54
이제호 <78259314+jhl8109@users.noreply.github.com>
2024-07-02 22:23:36 +0900

    [#15] feat : PDF 식별 기능 Controller 레이어 API 추가 및 통합 테스트 코드 작성

--- src/test/kotlin/zzibu/jeho/tagify/TagControllerTest.kt
+++ src/test/kotlin/zzibu/jeho/tagify/TagControllerTest.kt
@@ -17,7 +17,8 @@ import zzibu.jeho.tagify.controller.TagController
 import zzibu.jeho.tagify.exception.ErrorCode
 import zzibu.jeho.tagify.exception.ErrorResponse
 import zzibu.jeho.tagify.exception.GlobalExceptionHandler
-import zzibu.jeho.tagify.service.TagService
+import zzibu.jeho.tagify.service.ImageService
+import zzibu.jeho.tagify.service.PdfService
 import java.nio.file.Files
 import java.nio.file.Paths
 
@@ -29,7 +30,10 @@ class TagControllerTest : BehaviorSpec() {
     protected lateinit var mockMvc : MockMvc
 
     @Autowired
-    lateinit var tagService: TagService
+    lateinit var pdfService: PdfService
+
+    @Autowired
+    lateinit var imageService: ImageService
 
     @Autowired
     lateinit var objectMapper: ObjectMapper
@@ -37,7 +41,7 @@ class TagControllerTest : BehaviorSpec() {
     init {
         beforeSpec {
             mockMvc = MockMvcBuilders
-                .standaloneSetup(TagController(tagService))
+                .standaloneSetup(TagController(pdfService,imageService))
                 .setControllerAdvice(GlobalExceptionHandler())
                 .build()
             objectMapper = ObjectMapper()
@@ -50,7 +54,7 @@ class TagControllerTest : BehaviorSpec() {
 
             `when`("클라이언트가 이미지를 업로드하면") {
                 val result = mockMvc.perform(
-                    MockMvcRequestBuilders.multipart("/api/tags/generate/image")
+                    MockMvcRequestBuilders.multipart("/api/v1/tag/image")
                         .file(image)
                         .contentType(MediaType.MULTIPART_FORM_DATA))
                     .andExpect(MockMvcResultMatchers.status().isOk)
@@ -68,8 +72,47 @@ class TagControllerTest : BehaviorSpec() {
                 val unsupportedImage = MockMultipartFile("image", "unsupported-image.txt", "text/plain", unsupportedImageContent)
 
                 val result = mockMvc.perform(
-                    MockMvcRequestBuilders.multipart("/api/tags/generate/image")
+                    MockMvcRequestBuilders.multipart("/api/v1/tag/image")
                         .file(unsupportedImage)
+                        .contentType(MediaType.MULTIPART_FORM_DATA)
+                )
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest)
+                    .andReturn()
+
+                then("응답은 에러 메시지를 포함해야 한다") {
+                    val response = objectMapper.readValue(result.response.contentAsString, ErrorResponse::class.java)
+                    response.code shouldBe ErrorCode.INVALID_FILE_TYPE
+                }
+            }
+        }
+
+        given("PDF에 대한 태그 생성이 필요할 때") {
+            val path = Paths.get("src/test/resources/test.pdf")
+            val imageContent = Files.readAllBytes(path)
+            val image = MockMultipartFile("pdf", "test.pdf", "application/pdf", imageContent)
+
+            `when`("클라이언트가 PDF를 업로드하면") {
+                val result = mockMvc.perform(
+                    MockMvcRequestBuilders.multipart("/api/v1/tag/pdf")
+                        .file(image)
+                        .contentType(MediaType.MULTIPART_FORM_DATA))
+                    .andExpect(MockMvcResultMatchers.status().isOk)
+                    .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                    .andReturn()
+
+                then("PDF 식별을 통해 생성된 태그 목록을 반환한다.") {
+                    val response = objectMapper.readValue(result.response.contentAsString, List::class.java)
+
+                    response.size shouldBe 5
+                }
+            }
+            `when`("클라이언트가 지원되지 않는 PDF 포맷을 업로드하면") {
+                val unsupportedPdfContent = Files.readAllBytes(Paths.get("src/test/resources/unsupported-pdf.txt"))
+                val unsupportedPdf = MockMultipartFile("pdf", "unsupported-pdf.txt", "text/plain", unsupportedPdfContent)
+
+                val result = mockMvc.perform(
+                    MockMvcRequestBuilders.multipart("/api/v1/tag/pdf")
+                        .file(unsupportedPdf)
                         .contentType(MediaType.MULTIPART_FORM_DATA)
                 )
                     .andExpect(MockMvcResultMatchers.status().isBadRequest)
